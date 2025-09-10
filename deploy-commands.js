495254956fc5fd2d2f5d51a5aea20ce609bfc21d
Barbara Dorneles <barbara.laisdorneles@gmail.com>
2025-09-10 17:53:28 -0300

    try viserion on your DS server!

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const commands = [
   new SlashCommandBuilder()
     .setName('frozen')
-    .setDescription('Receba um fato sobre Game of Thrones de Viserion')
+    .setDescription('Receive a fact about Game of Thrones of Viserion')
     .addStringOption(option =>
       option.setName('mind')
         .setDescription('Wich mind?')
@@ -18,15 +18,15 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
-    console.log('Registrando slash commands...');
+    console.log('Registring slash commands...');
 
     await rest.put(
       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: commands }
     );
 
-    console.log('✅ Comandos registrados com sucesso!');
+    console.log('✅ Registry commands sucess!');
   } catch (error) {
-    console.error('Erro ao registrar comandos:', error);
+    console.error('Error registring command :', error);
   }
 })();
